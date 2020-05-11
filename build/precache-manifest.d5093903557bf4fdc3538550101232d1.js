21cfd56b4fc22295ba5e58b7547e41f2ed73e5af
Sergey Kalenikov <sergeykalenikov@gmail.com>
2020-05-11 17:01:25 +0300

    new build

---
+++ build/precache-manifest.d5093903557bf4fdc3538550101232d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0097abf464441d4f2728496098d5e76",
+    "url": "/random-cards-react/index.html"
+  },
+  {
+    "revision": "8a9054f838b7a7be7983",
+    "url": "/random-cards-react/static/css/main.a6877369.chunk.css"
+  },
+  {
+    "revision": "abf88d1b4482fd9a93c8",
+    "url": "/random-cards-react/static/js/2.f62f82dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/random-cards-react/static/js/2.f62f82dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9054f838b7a7be7983",
+    "url": "/random-cards-react/static/js/main.a0243021.chunk.js"
+  },
+  {
+    "revision": "128aac5742f0321f3f3b",
+    "url": "/random-cards-react/static/js/runtime-main.a23bcfb0.js"
+  }
+]);
